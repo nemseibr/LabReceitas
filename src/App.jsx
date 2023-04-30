c13ff7c7e3ddb7a525a579523e33e1911e037da0
Otávio Nascimento <116135293+nemseibr@users.noreply.github.com>
2023-04-30 06:47:13 -0300

    Update App.jsx
    Botão "Adicionar" alterado para "Salvar". Função salvar edição incluída.

--- src/App.jsx
+++ src/App.jsx
@@ -22,21 +22,33 @@ function App() {
 
   const handleAddRecipe = (event) => {
     event.preventDefault();
-    setRecipes([...recipes, newRecipe]);
+  
+    const storedRecipes = JSON.parse(localStorage.getItem("recipes")) || [];
+  
+    const recipeIndex = storedRecipes.findIndex(
+      (recipe) => recipe.name === newRecipe.name
+    );
+  
+    if (recipeIndex === -1) {
+      const newRecipes = [...storedRecipes, newRecipe];
+      setRecipes(newRecipes);
+      localStorage.setItem("recipes", JSON.stringify(newRecipes));
+    } else {
+      const newRecipes = [...storedRecipes];
+      newRecipes[recipeIndex] = newRecipe;
+      setRecipes(newRecipes);
+      localStorage.setItem("recipes", JSON.stringify(newRecipes));
+    }
+  
     setNewRecipe({
       name: "",
       ingredients: "",
       instructions: "",
       restrictions: [],
-      });
-      setShowAddRecipe(false);
-      };
+    });
+    setShowAddRecipe(false);
+  };
       
-      const handleEditRecipe = (index, updatedRecipe) => {
-      const newRecipes = [...recipes];
-      newRecipes[index] = updatedRecipe;
-      setRecipes(newRecipes);
-      };
       
       const handleDeleteRecipe = (index) => {
       const newRecipes = [...recipes];
@@ -58,85 +70,82 @@ function App() {
       }
       });
       
-      return (
-      <div className="container">
-      <h1>Seja bem vindo ao Lab Receitas!</h1>
-      <div className="header">
+return (
+  <div className="container">
+    <h1>Seja bem vindo ao Lab Receitas!</h1>
+    <div className="header">
       <h2>Tenha todas as suas receitas na palma da mão</h2>
       <img src="/logolabreceitas.png" alt="LogoLabReceitas" style={{ width: '200px', height: '200px' }} />
       <p>Cadastre, exclua e edite! Aproveite!</p>
       <p>Obs: Para filtrar as receitas que não possuem Glúten e Lactose, selecione a opção "Todas as Receitas". </p>
-      </div>
-      <div className="filter">
-  <input type="radio" id="filter-gluten" name="filter" value="Sem Glúten" checked={filter === "Sem Glúten"} onChange={handleFilterChange} />      
-  <label htmlFor="filter-gluten">Sem Glúten</label>
-  
-  <input type="radio" id="filter-lactose" name="filter" value="Sem Lactose" checked={filter === "Sem Lactose"} onChange={handleFilterChange} />
-  <label htmlFor="filter-lactose">Sem Lactose</label>
+    </div>
+    <div className="filter">
+      <input type="radio" id="filter-gluten" name="filter" value="Sem Glúten" checked={filter === "Sem Glúten"} onChange={handleFilterChange} />      
+      <label htmlFor="filter-gluten">Sem Glúten</label>
+      
+      <input type="radio" id="filter-lactose" name="filter" value="Sem Lactose" checked={filter === "Sem Lactose"} onChange={handleFilterChange} />
+      <label htmlFor="filter-lactose">Sem Lactose</label>
 
-  <input type="radio" id="filter-all" name="filter" value="Todas as receitas" checked={filter === "Todas as receitas"} onChange={handleFilterChange} />
-  <label htmlFor="filter-all">Todas as receitas</label>
- 
-</div>
-      <div>
+      <input type="radio" id="filter-all" name="filter" value="Todas as receitas" checked={filter === "Todas as receitas"} onChange={handleFilterChange} />
+      <label htmlFor="filter-all">Todas as receitas</label>
+    </div>
+    <div>
       {filteredRecipes.map((recipe, index) => (
-      <div key={index} className="recipe">
-      <h2>{recipe.name}</h2>
-      <p>{recipe.ingredients}</p>
-      <p>{recipe.instructions}</p>
-      <p>{recipe.restrictions.join(", ")}</p>
-      <button
-      className="button"
-      onClick={() => {
-      const updatedRecipe = prompt("Editar receita", JSON.stringify(recipe));
-      if (updatedRecipe) {
-      handleEditRecipe(index, JSON.parse(updatedRecipe));
-      }
-      }}
-      >
-      ! Editar
-      </button>
-      <button className="button" onClick={() => handleDeleteRecipe(index)}>
-      X Excluir
-      </button>
-      </div>
+        <div key={index} className="recipe">
+          <h2>{recipe.name}</h2>
+          <p>{recipe.ingredients}</p>
+          <p>{recipe.instructions}</p>
+          <p>{recipe.restrictions.join(", ")}</p>
+          <button
+            className="button"
+            onClick={() => {
+              setShowAddRecipe(true);
+              setNewRecipe(recipe);
+            }}
+          >
+            ! Editar
+          </button>
+          <button className="button" onClick={() => handleDeleteRecipe(index)}>
+            X Excluir
+          </button>
+        </div>
       ))}
-      </div>
-      <button
+    </div>
+    <button
       className="button"
       onClick={() => setShowAddRecipe(true)}
       style={{ marginTop: "20px" }}
-      >
+    >
       Adicionar Receita
-      </button>
-      <div className="add-recipe" style={{ display: showAddRecipe ? "flex" : "none" }}>
+    </button>
+    <div className="add-recipe" style={{ display: showAddRecipe ? "flex" : "none" }}>
       <form onSubmit={handleAddRecipe}>
-      <label htmlFor="new-recipe-name">Nome:</label>
-      <input
-      id="new-recipe-name"
-      value={newRecipe.name}
-      onChange={(event) => setNewRecipe({ ...newRecipe, name: event.target.value })}
-      required
-      />
-      <label htmlFor="new-recipe-ingredients">Ingredientes:</label>
-      <textarea
-      id="new-recipe-ingredients"
-      value={newRecipe.ingredients}
-      onChange={(event) =>
-      setNewRecipe({ ...newRecipe,ingredients: event.target.value })
-    }
-    required
-    />
-    <label htmlFor="new-recipe-instructions">Modo de Preparo:</label>
-    <textarea
-    id="new-recipe-instructions"
-    value={newRecipe.instructions}
-    onChange={(event) =>
-    setNewRecipe({ ...newRecipe, instructions: event.target.value })
-    }
-    required
-    />
-    <label htmlFor="new-recipe-restrictions">Restrições</label>
+        <label htmlFor="new-recipe-name">Nome:</label>
+        <input
+          id="new-recipe-name"
+          value={newRecipe.name}
+          onChange={(event) => setNewRecipe({ ...newRecipe, name: event.target.value })}
+          required
+        />
+        <label htmlFor="new-recipe-ingredients">Ingredientes:</label>
+        <textarea
+          id="new-recipe-ingredients"
+          value={newRecipe.ingredients}
+          onChange={(event) =>
+            setNewRecipe({ ...newRecipe,ingredients: event.target.value })
+          }
+          required
+        />
+        <label htmlFor="new-recipe-instructions">Modo de Preparo:</label>
+        <textarea
+          id="new-recipe-instructions"
+          value={newRecipe.instructions}
+          onChange={(event) =>
+            setNewRecipe({ ...newRecipe, instructions: event.target.value })
+          }
+          required
+        />
+        <label htmlFor="new-recipe-restrictions">Restrições</label>
     <div className="restrictions">
   
     <label htmlFor="Sem Glúten">Sem Glúten:</label>
@@ -170,7 +179,7 @@ function App() {
 </div>
     <div className="add-recipe-buttons">
     <button className="button" type="submit">
-    Adicionar
+    Salvar
     </button>
     <button
     className="button"
